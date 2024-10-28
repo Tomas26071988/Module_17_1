3170025895135b15fde803bdad1b19496197d49c
Tomas26071988 <artem_1988_artem1988@icloud.com>
2024-10-28 13:22:44 +0300

    Add files via upload

---
+++ user.py
@@ -0,0 +1,30 @@
+
+from fastapi import APIRouter
+from schemas import CreateUser, UpdateUser
+
+router = APIRouter(prefix="/user", tags=["user"])
+
+
+@router.get("/")
+async def all_users():
+    pass
+
+
+@router.get("/{user_id}")
+async def user_by_id(user_id: int):
+    pass
+
+
+@router.post("/create")
+async def create_user(user: CreateUser):
+    pass
+
+
+@router.put("/update")
+async def update_user(user_id: int, user: UpdateUser):
+    pass
+
+
+@router.delete("/delete")
+async def delete_user(user_id: int):
+    pass
